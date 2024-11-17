657fe6861ea5bfd1c25d67034024b9aca56a96ef
Sai <saisrinivas.samoju@gmail.com>
2024-11-17 19:28:13 +0530

    Update version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logitest',
-    version='0.1.0',
+    version='0.1.1',
     description='Automated test case generation tool for Python modules.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
