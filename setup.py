9d1f973ca61d64bf3ea20815f5a5786d76e4ac12
Sai <saisrinivas.samoju@gmail.com>
2024-11-19 03:06:37 +0530

    Update version to 0.1.3 - Update README.md file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logitest',
-    version='0.1.2',
+    version='0.1.3',
     description='Automated test case generation tool for Python modules.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
